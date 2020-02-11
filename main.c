73d883a579e384886afbd0b25af438edd05b1198
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-02-11 14:02:17 -0300

    Calculadora de médias/2

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nome [40];
+    float nota1;
+    float nota2;
+    float media;
+
+    printf ("Qual o seu nome?: ");
+    scanf ("%s", nome);
+
+    printf ("Qual sua primeira nota?: ");
+    scanf ("%f", &nota1);
+
+    printf ("Qual a sua segunda nota?:");
+    scanf ("%f", &nota2);
+
+    media = (nota1 + nota2)/2;
+
+    printf ("\n%s sua media e: %f", nome, media);
+    }
