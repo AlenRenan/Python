09c8c69a5a2ecfe8867099780ff367f34596d365
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-02-11 15:05:25 -0300

    Rename desafio.c to Repetição.c

--- Repetição.c
+++ Repetição.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 int main(){
-    //Repetição com o while
+    //RepetiÃ§Ã£o com o while
     cont = 0;
     while (cont <10) {
         printf ("\n Aluno %d", (cont+1))
         cont++
     }
 }
-    //repetição com o for
+    //repetiÃ§Ã£o com o for
     for (cont=0; cont<10:cont++){
         printf ("\n Aluno %d", (cont+1));
     }
-    //Repetição com do-while
+    //RepetiÃ§Ã£o com do-while
     cont = 0;
     do {
         printf ("\n Aluno %d", (cont+1));
         while(cont<10);
     }
 
-    //Começo do desafio
+    //ComeÃ§o do desafio
 
 
 
