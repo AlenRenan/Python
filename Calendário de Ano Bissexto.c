d2d0da4f7a23e943f48f752e50b58cb615af3744
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-02-14 17:53:22 -0300

    Calendário de Ano Bissexto.c

---
+++ Calendário de Ano Bissexto.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ano;
+    int divisao1;
+    int divisao2;
+    int bano;
+
+    printf ("\nInsira o ano: ");
+        scanf ("%d", &ano);
+
+    divisao1 = ano % 4;
+    divisao2 = ano % 100;
+
+    if (divisao1 <= 0){
+        printf ("\nO ano de %d e um ano bissexto", ano);
+    if (divisao2 <= 0){
+        printf ("\nO ano de %d e um ano bissexto", ano);
+    }
+    }
+    else
+        printf ("\n o ano de %d nao e um ano bissexto", ano);
+    }
