f3e4bec0237c089f09bf1e0cc710a78b489c8fc6
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-03-06 16:15:49 -0300

    Update PÁSCOA.c

--- PÁSCOA.c
+++ PÁSCOA.c
@@ -2,35 +2,24 @@ int main (){
 
    int ano;
    int a, b,c, d, e;
-    int pascoa;
-  int mpascoa;
+   int pascoa;
+   int mpascoa;
+   
   printf  ("Qual o ano?: ");
   scanf ("%d", &ano);
-
-
+   
   a = ano % 19;
-
   b = ano % 4;
-
   c = ano % 7;
-
   d = (19*a + 24)% 30;
-
   e = ((2 * b) + (4 * c) + (6 * d) + 5) % 7;
-
+   
   if (d + e > 9){
         pascoa = d + e - 9;
         mpascoa = 4;
-
   } else if (d + e <= 9){
       pascoa = d + e + 22;0
       mpascoa = 3;
-
   }
   printf ("A pascoa sera no dia %d, e no mes %d", pascoa, mpascoa);
-
-
-
-
-
 }
